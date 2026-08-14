9d0352c58b91100b26f0d8eefb7b4cc76dbbd431
sakshitulsyan <tulsyansakshi699@gmail.com>
2026-08-14 11:29:56 +0530

    arrsumm

---
+++ DAY3/arrsum.js
@@ -0,0 +1,14 @@
+let students = [
+    ["Sakshi", "Tulsyan", 18, 90],
+    ["Sam", "Sharma", 19, 85],
+    ["Shiva", "Singh", 18, 95]
+];
+
+for (let i = 0; i < students.length; i++) {
+    console.log(
+        "FirstName:", students[i][0],
+        "LastName:", students[i][1],
+        "Age:", students[i][2],
+        "Marks:", students[i][3]
+    );
+}
