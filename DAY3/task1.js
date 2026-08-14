635b51ee4b5cb7df6568c8b24053215564d83b76
sakshitulsyan <tulsyansakshi699@gmail.com>
2026-08-14 11:12:37 +0530

    task1

---
+++ DAY3/task1.js
@@ -0,0 +1,9 @@
+let Name = "Sakshi Tulsyan";
+let id = "2503215300166";
+let Branch = "CSE";
+let Department = "AIML";
+
+console.log("Name:", Name);
+console.log("ID:", id);
+console.log("Branch:", Branch);
+console.log("Department:", Department);
