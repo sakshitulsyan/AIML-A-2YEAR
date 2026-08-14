7e4960339b15b5498fc31bd64e9da79075d23e6f
sakshitulsyan <tulsyansakshi699@gmail.com>
2026-08-14 11:45:23 +0530

    arr

---
+++ DAY3/arr.js
@@ -0,0 +1,19 @@
+const studs = [
+    {
+        Name: "Sakshi",
+        Age: 18,
+        College: "ABES Engineering College"
+    },
+    {
+        Name: "Sam",
+        Age: 19,
+        College: "ABES Engineering College"
+    },
+    {
+        Name: "Shiva",
+        Age: 18,
+        College: "ABES Engineering College"
+    }
+];
+
+console.log(students);
