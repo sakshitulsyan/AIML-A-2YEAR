08899ba2e670b383f46e49b46a5ef017a8cf0f9d
sakshitulsyan <tulsyansakshi699@gmail.com>
2026-08-19 13:41:13 +0530

    arrr.js

---
+++ DAY4/arrr.js
@@ -0,0 +1,50 @@
+console.log("==== Sring Object ====");
+
+let name = "Sakshi";
+console.log(name);
+
+//String Length
+
+console.log("Length = ",name.length);
+
+//--------------------------------
+// String Methods
+//--------------------------------
+
+console.log("\n==== String Methods ====");
+
+let str="Welcome JavaScript";
+
+//1.toUpperCase()
+
+console.log("Upper Case = ",str.toUpperCase());
+
+//2.toLowerCase()
+
+console.log("Lower Case = ",str.toLowerCase());
+
+//3. charAt()
+
+console.log("Character at inder 3 = ",str[2]);
+
+//4. indexOf()
+
+console.log("Index of Java = ",str.indexOf("Java"));
+
+//5. lastIndex()
+
+let message = "JavaScript is easy. JavaScript is powerful";
+
+console.log(
+    "Last Index = ",
+    message.lastIndexOf("JavaScript")
+);
+
+
+
+
+
+
+
+
+
